0c8521b67b76c88ceb14833f581a22480f41c8d1
shaflynurrasyid <mshaflyn24@gmail.com>
2023-11-30 20:11:35 +0700

    update fiction nonfiction

--- frontend/src/components/Aside.js
+++ frontend/src/components/Aside.js
@@ -8,14 +8,14 @@ const Aside = {
   <div class="aside-body">
     <ul class="categories">
       <li>
-        <a href="/#/?q=shirt"
-          >Shirts
+        <a href="/#/?q=fiction"
+          >Fiction
           <span><i class="fa fa-chevron-right"></i></span>
         </a>
       </li>
       <li>
-        <a href="/#/?q=pant"
-          >Pants
+        <a href="/#/?q=non-fiction"
+          >Non-Fiction
           <span><i class="fa fa-chevron-right"></i></span>
         </a>
       </li> 
